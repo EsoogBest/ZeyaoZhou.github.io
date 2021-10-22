e6e98e8d0219fbf4c0ef40590bbcb991831824e4
ZeyaoZhou <92761028+ZeyaoZhou@users.noreply.github.com>
2021-10-22 11:42:08 -1000

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function name() {
+function userName() {
   //get value from input field and save as "a"
   var a = document.getElementById("input").value;
   //create LS key with value of "a"
@@ -7,7 +7,7 @@ function name() {
   location.href = "two.html";
   }
 
-functions functionTwo() {
+function functionTwo() {
   var b = localStorage.getItem("uName");
   document.getElementById("placeholder").innerHTML = "Hello" = b;
 
