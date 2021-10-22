ab9117beb8586b80c2008a0a1c8db08021e2a77f
ZeyaoZhou <92761028+ZeyaoZhou@users.noreply.github.com>
2021-10-22 11:36:23 -1000

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function name() {
   //get value from input field and save as "a"
-  var a = document.getElementById("nameInput").value;
+  var a = document.getElementById("input").value;
   //create LS key with value of "a"
   localStorage.setItem("uName",a);
   //automatically take user to two.html
